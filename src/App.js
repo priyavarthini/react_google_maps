1c91a57edbd683fd3888f9839078b0adcbe34b81
priyavarthini <preethivarthini@gmail.com>
2018-07-21 23:21:02 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import "./App.css";
+import Map from "./Map";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentLatLng: {
+        lat: 1.3467182,
+        lng: 103.8496971
+      },
+      isMarkerShown: false
+    };
+  }
+
+  showCurrentLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log(position.coords);
+        this.setState(prevState => ({
+          currentLatLng: {
+            ...prevState.currentLatLng,
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          },
+          isMarkerShown: true
+        }));
+      });
+    } else {
+      error => console.log(error);
+    }
+  };
+
+  componentDidMount() {
+    this.showCurrentLocation();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Restaurants Nearby App</h1>
+          <p> By Priyavarthini </p>
+        </header>
+        <Map
+          isMarkerShown={this.state.isMarkerShown}
+          currentLocation={this.state.currentLatLng}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
+//render(<App />, document.getElementById("root"));
